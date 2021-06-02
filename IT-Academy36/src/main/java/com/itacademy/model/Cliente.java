bfdf39dec76e5138d17317624626f9ccfda71775
petrusCode <pedro_hot32@hotmail.com>
2021-06-02 11:30:39 +0200

    Class Cuenta and Cliente implemented

--- IT-Academy36/src/main/java/com/itacademy/model/Cliente.java
+++ IT-Academy36/src/main/java/com/itacademy/model/Cliente.java
@@ -1,5 +1,22 @@
 package com.itacademy.model;
 
-public class Cliente {
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+public class Cliente {
+	/*
+	 * Programa una aplicación que tenga programadas todas las opciones del
+	 * siguiente menú:
+	 * 
+	 * 0.- Salir de la aplicación 1.- Crear cliente 2.- Mostrar cliente 3.-
+	 * Eliminar cliente 4.- Crear cuenta de un cliente 5.- Mostrar una cuenta de
+	 * un cliente 6.- Mostrar las cuentas de un cliente 7.- Eliminar una cuenta
+	 * de un cliente 8.- Ingresar euros en una cuenta de un cliente 9.- Retirar
+	 * euros en una cuenta de un cliente
+	 */
+
+	private String nombre;
+	private String apellido;
 }
--- IT-Academy36/src/main/java/com/itacademy/model/Cuenta.java
+++ IT-Academy36/src/main/java/com/itacademy/model/Cuenta.java
@@ -1,5 +1,24 @@
 package com.itacademy.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Cuenta {
 
+	private Integer numCuenta;
+	private Integer saldo;
+
+	int ingresar(Integer cantidad) {
+
+		return cantidad;
+
+	}
+
+	int retirar(Integer cantidad) {
+
+		return cantidad;
+
+	}
 }
